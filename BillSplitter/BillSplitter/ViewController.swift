91d88e051cd710b429d073eeb276ca1e9d349274
Mostafa AlBelliehy <mst.dev@gmail.com>
2018-10-12 18:36:24 +0200

    Used optional binding in view controller (with comments about why).

--- BillSplitter/BillSplitter/ViewController.swift
+++ BillSplitter/BillSplitter/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	@IBOutlet weak var dinersTF: UITextField!
+	@IBOutlet weak var totalCostTF: UITextField!
+	@IBOutlet weak var resultLbl: UILabel!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	@IBAction func calculate(_ sender: Any) {
+		
+		let dinerValue = dinersTF.text;
+		let totalValue = totalCostTF.text;
+		
+		// Used optional binding because TF.text is optional
+		if let dinerStr = dinerValue, let totalStr = totalValue
+		{
+			
+			let dinerDouble = Double(dinerStr);
+			let totalDouble = Double(totalStr);
+			
+			// Used optional binding because Double() returns an optional
+			if let diner = dinerDouble, let total = totalDouble
+			{
+				print(diner + total);
+			}
+		}
+		
 	}
-
-
 }
 
