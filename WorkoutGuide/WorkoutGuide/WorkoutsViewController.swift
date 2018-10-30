68093455d35ad97bba90d39cbd8dabae742318ae
Mostafa AlBelliehy <mst.dev@gmail.com>
2018-10-30 16:42:37 +0200

    184. Dynamically updating title from plist

--- WorkoutGuide/WorkoutGuide/WorkoutsViewController.swift
+++ WorkoutGuide/WorkoutGuide/WorkoutsViewController.swift
@@ -13,29 +13,8 @@ class WorkoutsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 		
-		// Applying styles to certain controls
-		// Accessing controls on the view
-		let allViews = view.subviews;
+		self.title = "Choose Workout";
 		
-		for subView in allViews
-		{
-			if subView.tag == 10 //is Button
-				// 'is' here checks the class of the view (distinguish controls by type)
-				// tag is used to distinguish specific controls
-			{
-				// Accessing the control
-				let control = subView as! UIButton		// I know it's a button
-				
-				// Styling
-				control.setTitleColor(UIColor.white, for: .normal);
-				control.titleLabel?.font = UIFont(name: "HelveticaNeue-CondensedBlack", size: 20);
-				control.layer.borderColor = UIColor(red: 0.99, green: 0.29, blue: 0.34, alpha: 1.00).cgColor;
-				control.layer.borderWidth = 1.5;
-				control.layer.cornerRadius = 5;
-				control.clipsToBounds = true;
-			}
-		}
-
 		// Path to the PList file
 		let path = Bundle.main.path(forResource: "Workouts", ofType: "plist");
 		
@@ -45,7 +24,33 @@ class WorkoutsViewController: UIViewController {
 			
 			if let validDict = dict
 			{
-				print(validDict);
+				// Extracting dictionary in an array
+				let titles = validDict.allKeys;
+
+				/// Accessing all buttons
+				let allViews = view.subviews;
+				
+				for i in 0..<allViews.count
+				{
+					if allViews[i].tag == 10 //is Button
+						// 'is' here checks the class of the view (distinguish controls by type)
+						// tag is used to distinguish specific controls
+					{
+						// Accessing the control
+						let control = allViews[i] as! UIButton		// I know it's a button
+						
+						// Button title
+						control.setTitle(titles[i] as? String, for: .normal);
+						
+						// Styling
+						control.setTitleColor(UIColor(red: 0.99, green: 0.29, blue: 0.34, alpha: 1.00), for: .normal);
+						control.titleLabel?.font = UIFont(name: "HelveticaNeue-CondensedBlack", size: 20);
+						control.layer.borderColor = UIColor(red: 0.99, green: 0.29, blue: 0.34, alpha: 1.00).cgColor;
+						control.layer.borderWidth = 1.5;
+						control.layer.cornerRadius = 5;
+						control.clipsToBounds = true;
+					}
+				}
 			}
 		}
     }
