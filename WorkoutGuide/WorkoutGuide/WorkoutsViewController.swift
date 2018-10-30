2036530174f1a79fcd29874e329214e9ad9bedf6
Mostafa AlBelliehy <mst.dev@gmail.com>
2018-10-30 16:11:49 +0200

    183. Using tags, class type and subview properties to customise views

--- WorkoutGuide/WorkoutGuide/WorkoutsViewController.swift
+++ WorkoutGuide/WorkoutGuide/WorkoutsViewController.swift
@@ -12,6 +12,29 @@ class WorkoutsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+		
+		// Applying styles to certain controls
+		// Accessing controls on the view
+		let allViews = view.subviews;
+		
+		for subView in allViews
+		{
+			if subView.tag == 10 //is Button
+				// 'is' here checks the class of the view (distinguish controls by type)
+				// tag is used to distinguish specific controls
+			{
+				// Accessing the control
+				let control = subView as! UIButton		// I know it's a button
+				
+				// Styling
+				control.setTitleColor(UIColor.white, for: .normal);
+				control.titleLabel?.font = UIFont(name: "HelveticaNeue-CondensedBlack", size: 20);
+				control.layer.borderColor = UIColor(red: 0.99, green: 0.29, blue: 0.34, alpha: 1.00).cgColor;
+				control.layer.borderWidth = 1.5;
+				control.layer.cornerRadius = 5;
+				control.clipsToBounds = true;
+			}
+		}
 
 		// Path to the PList file
 		let path = Bundle.main.path(forResource: "Workouts", ofType: "plist");
