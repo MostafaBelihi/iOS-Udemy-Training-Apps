88e78c562562c805fffef43bcc6f348a0908d206
Mostafa AlBelliehy <mst.dev@gmail.com>
2018-11-17 11:12:49 +0200

    V.204: Adding actions and outlets.

---
+++ FaveCar/FaveCar/AddCarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddCarViewController.swift
+//  FaveCar
+//
+//  Created by Mostafa AlBelliehy on 13/11/2018.
+//  Copyright © 2018 Mostafa AlBelliehy. All rights reserved.
+//
+
+import UIKit
+
+class AddCarViewController: UIViewController {
+
+	@IBOutlet weak var txtCarName: UITextField!
+	@IBOutlet weak var imgCarImage: UIImageView!
+	@IBOutlet weak var btnSelectCar: UIButton!
+	@IBOutlet weak var lblCharacterCount: UILabel!
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+	@IBAction func selectCar(_ sender: Any) {
+	}
+	
+	@IBAction func save(_ sender: Any) {
+	}
+	
+}
