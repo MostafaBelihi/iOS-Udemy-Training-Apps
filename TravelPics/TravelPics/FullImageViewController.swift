340afa372f0c8d256ce1afd365583b6b647b6228
Mostafa AlBelliehy <mst.dev@gmail.com>
2018-11-24 11:39:45 +0200

    224. How to zoom on images + UI tweaks

--- TravelPics/TravelPics/FullImageViewController.swift
+++ TravelPics/TravelPics/FullImageViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FullImageViewController: UIViewController {
+class FullImageViewController: UIViewController, UIScrollViewDelegate {
 	
 	var fullImage = UIImage(named: "1");
 
@@ -17,8 +17,21 @@ class FullImageViewController: UIViewController {
 	
 	override func viewDidLoad() {
         super.viewDidLoad()
+		
+		scrollView.delegate = self;
 
+		// Load image
 		imgFullImage.image = fullImage;
+		
+		// Configure zoom
+		scrollView.minimumZoomScale = 1;
+		scrollView.maximumZoomScale = 4;
+		scrollView.contentSize = imgFullImage.frame.size;
     }
+	
+	// Image to zoom
+	func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+		return imgFullImage;
+	}
 
 }
